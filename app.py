64279f66d24f317aee7e01e2793860264618604b
MDigiTechnology <mdigitechnology@gmail.com>
2025-03-08 04:23:31 +0530

    Simplified Flask app and updated dependencies

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html', title='Home')
-
-@app.route('/about')
-def about():
-    return render_template('about.html', title='About')
+def hello():
+    return "Hello, World! This is a simple Flask app running on Railway.com"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False) 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=False) 
